eb7227818b7a2f00fffa50f3969a59e8ae8fa4c6
ricepotato <ricepotato40@gmail.com>
2026-06-06 15:50:16 +0900

    feat: update primary color in brand configuration

--- granite.config.ts
+++ granite.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   appName: "fuel-log-ait",
   brand: {
     displayName: "주유기록", // 화면에 노출될 앱의 한글 이름으로 바꿔주세요.
-    primaryColor: "#64B5F6", // 화면에 노출될 앱의 기본 색상으로 바꿔주세요.
+    primaryColor: "#3182F6", // 화면에 노출될 앱의 기본 색상으로 바꿔주세요.
     icon: "", // 화면에 노출될 앱의 아이콘 이미지 주소로 바꿔주세요.
   },
   web: {
@@ -14,6 +14,9 @@ export default defineConfig({
       dev: "vite dev",
       build: "vite build",
     },
+  },
+  webViewProps: {
+    pullToRefreshEnabled: false,
   },
   navigationBar: {
     withBackButton: true,
